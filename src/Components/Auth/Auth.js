975d8ac753158383595d614080eb1cde3e1970ee
Eli-Smith <elijah.smith23@yahoo.com>
2019-11-04 13:23:33 -0500

    Comments, Clean up and Styling

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -12,7 +12,7 @@ const Auth = (props) => {
         <div className='mainAuth'>
             <div className='mainAuthDiv'>
                 <h1 style={{margin: '0px'}}>Welcome to Hike Indy!</h1>
-                <h4 style={{marginTop: '0px'}}>Keep track of trails you've visited, rate your explored trails 
+                <h4 style={{marginTop: '0px', marginRight: '1em', marginLeft: '1em'}}>Keep track of trails you've visited, rate your explored trails 
                     <br/>
                     or find your next hike from over 200 Indiana hiking trails!</h4>
                 <div className='formDiv'>
